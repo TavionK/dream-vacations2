dde668e1aea36b25d27a69e538fddf927c62b28a
TavionK <tavion.k.britt99@gmail.com>
2026-03-24 22:22:14 -0400

    Refactor global styles with CSS layers and update Home page layout with "Coming Soon" content and logo asset

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main><h1>Dream Vacations 2</h1></main>
+    <main className={"h-dvh flex flex-col gap-0 justify-center items-center"}>
+      <Image
+        src="/images/Dreamvacations2.png"
+        className="block mx-auto"
+        alt="Dream Vacations 2 logo"
+        width={500}
+        height={500}
+      />
+      <h1 className={"text-2xl font-geist"}>Coming Soon</h1>
+    </main>
   );
 }
