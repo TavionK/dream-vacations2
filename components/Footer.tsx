bb1abb48c4e9fd4018c3fdc05a2c021040ef00df
TavionK <tavion.k.britt99@gmail.com>
2026-03-24 22:46:10 -0400

    Update Footer text styling to use uppercase format

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <p className={"text-center text-sm py-4"}>
+    <p className={"text-center text-sm uppercase py-4"}>
       &copy; 2026 Dream Vacations 2 &middot; Travel Is My Business
     </p>
   );
