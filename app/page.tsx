b86c8bcf524a3b03dc1a36d135b1a3050da5e90c
TavionK <tavion.k.britt99@gmail.com>
2026-03-27 14:01:44 -0400

    Add Dreamvacations logo SVG to public assets directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
+import Image from "next/image";
+import React from "react";
+
 export default function Home() {
   return (
     <main className="flex flex-col flex-1 gap-0 justify-center items-center px-4 text-center">
+      <div className="relative w-50 h-50">
+        <Image
+          src="/images/Dreamvacations2.svg"
+          alt="Logo"
+          fill
+          loading="eager"
+        />
+      </div>
       <h1 className="text-5xl font-semibold mb-2">Coming Soon</h1>
       <p className="uppercase">Your next adventure awaits</p>
     </main>
